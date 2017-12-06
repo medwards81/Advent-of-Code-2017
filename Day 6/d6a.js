557f77d6ac5691f511891f4c0bfbda254a2aba2f
Marc Edwards <mdouglasedwards@gmail.com>
2017-12-06 14:23:43 -0500

    Add Day 6

---
+++ Day 6/d6a.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+
+let banksStrArr = fs
+  .readFileSync('d6a.txt')
+  .toString()
+  .trim()
+  .split(/\s+/);
+//banksStrArr = ['0', '2', '7', '0'];
+const banks = banksStrArr.map(bank => parseInt(bank));
+
+console.log({ banks });
+
+const findMax = ele => ele === curMax;
+
+const maxIdx = banks.length - 1;
+let curMax, curMaxIdx, nextBankIdx;
+
+let prevConfFound = false;
+let redistributions = {};
+let maxIter = 999999;
+let counter = 0;
+let redistributionKey;
+
+while (!prevConfFound) {
+  counter++;
+  curMax = Math.max(...banks);
+  curMaxIdx = banks.findIndex(findMax);
+  banks[curMaxIdx] = 0;
+  nextBankIdx = curMaxIdx + 1;
+  while (curMax > 0) {
+    if (nextBankIdx > maxIdx) nextBankIdx = 0;
+    banks[nextBankIdx]++;
+    nextBankIdx++;
+    curMax--;
+  }
+  console.log('redis', counter, ':', banks);
+  redistributionKey = banks.join('');
+  if (redistributions[redistributionKey]) {
+    console.log(
+      'found duplicate distribution',
+      banks,
+      'after',
+      counter,
+      'redistributions'
+    );
+    prevConfFound = true;
+  }
+  redistributions[redistributionKey] = 1;
+  if (counter >= maxIter) {
+    console.log('oops... something must be wrong. Aborting search.');
+    prevConfFound = true;
+  }
+}
+
+//console.log({ redistributions });
