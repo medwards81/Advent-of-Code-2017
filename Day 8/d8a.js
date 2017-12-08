882deb28cefd5d805fcd86f6b57e884d758c7b16
Marc Edwards <mdouglasedwards@gmail.com>
2017-12-08 09:01:00 -0500

    Add Day 8

---
+++ Day 8/d8a.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const instrs = fs
+  .readFileSync('input.txt')
+  .toString()
+  .trim()
+  .split(/\n/);
+
+let tracker = {};
+
+// b inc 5 if a > 1
+// a inc 1 if b < 5
+// c dec -10 if a >= 1
+// c inc -20 if c == 10
+
+const opMap = {
+  inc: '+=',
+  dec: '-='
+};
+
+instrs.forEach(instr => {
+  instr.trim();
+  //console.log(instr);
+  let [reg1, op, val, , reg2, comp, reg2val] = instr.split(/\s+/);
+  //console.log({ reg1, op, val, reg2, comp, reg2val });
+  //console.log(tracker[reg1]);
+  if (!(reg1 in tracker)) tracker[reg1] = 0;
+  if (!(reg2 in tracker)) tracker[reg2] = 0;
+  const evalScript = `if (tracker["${reg2}"] ${comp} ${reg2val}) tracker["${reg1}"] ${opMap[
+    op
+  ]} ${val};`;
+  console.log({ evalScript });
+  eval(evalScript);
+});
+
+//console.log({ tracker });
+let vals = [];
+for (register in tracker) {
+  vals.push(tracker[register]);
+}
+
+const highestVal = Math.max(...vals);
+console.log({ highestVal });
